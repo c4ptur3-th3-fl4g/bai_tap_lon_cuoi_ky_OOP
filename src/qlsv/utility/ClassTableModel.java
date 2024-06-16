94c9810cb22f9dda2bc654f3d6a60f7c7bfbf6a5
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2024-06-16 16:03:33 +0700

    chore: Update ClassTableModel to use consistent variable naming

--- src/qlsv/utility/ClassTableModel.java
+++ src/qlsv/utility/ClassTableModel.java
@@ -2,15 +2,15 @@ package qlsv.utility;
 
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
+import qlsv.model.GiangVien;
 import qlsv.model.SinhVien;
-
 /**
  *
  * @author trand
  */
 public class ClassTableModel {
     
-    public DefaultTableModel setTableSinhVien(List<SinhVien> listItem, String[] listColum) {
+    public DefaultTableModel setTableSinhVien(List<SinhVien> listItem, String[] listColumn) {
         DefaultTableModel dtm = new DefaultTableModel() {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -19,8 +19,8 @@ public class ClassTableModel {
             
         };
         
-        dtm.setColumnIdentifiers(listColum);
-        int columns = listColum.length;
+        dtm.setColumnIdentifiers(listColumn);
+        int columns = listColumn.length;
         Object[] obj = null;
         int rows = listItem.size();
         if(rows > 0) {
@@ -35,6 +35,39 @@ public class ClassTableModel {
                 obj[5] = sinhVien.getSo_dien_thoai();
                 obj[6] = sinhVien.getDia_chi();
                 obj[7] = sinhVien.isTinh_trang() == true ? "Đang học" : "Đã nghỉ học";
+                dtm.addRow(obj);
+            }
+        }
+
+
+        return dtm;
+    }
+
+    public DefaultTableModel setTableGiangVien(List<GiangVien> listItem, String[] listColumn) {
+        DefaultTableModel dtm = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+            
+        };
+        
+        dtm.setColumnIdentifiers(listColumn);
+        int columns = listColumn.length;
+        Object[] obj = null;
+        int rows = listItem.size();
+        if(rows > 0) {
+            for(int i = 0; i < rows; i++) {
+                GiangVien giangVien = listItem.get(i);
+                obj = new Object[columns];
+                obj[0] = (i + 1);
+                obj[1] = giangVien.getMa_giang_vien();
+                obj[2] = giangVien.getHo_ten();
+                obj[3] = giangVien.getNgay_sinh();
+                obj[4] = giangVien.isGioi_tinh() == true ? "Nam" : "Nữ";
+                obj[5] = giangVien.getSo_dien_thoai();
+                obj[6] = giangVien.getDia_chi();
+                obj[7] = giangVien.isTinh_trang() == true ? "Đang học" : "Đã nghỉ học";
                 dtm.addRow(obj);
             }
         }
